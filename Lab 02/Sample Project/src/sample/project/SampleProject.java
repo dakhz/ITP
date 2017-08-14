b5c70cede769ff23b40fcfcf139c675816774367
dakhz <30996112+dakhz@users.noreply.github.com>
2017-08-14 12:30:22 +0530

    Update SampleProject.java

--- Lab 02/Sample Project/src/sample/project/SampleProject.java
+++ Lab 02/Sample Project/src/sample/project/SampleProject.java
@@ -17,7 +17,7 @@ public class SampleProject {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("new!");
+        System.out.println("new2!");
     }
     
 }
